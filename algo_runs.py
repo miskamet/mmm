fix jackpot, add roas calculation

algo_runs.py
@@ -83,16 +83,17 @@ MAPE = mean_absolute_percentage_error(sales_scaled, y_fit)
 msqrt = root_mean_squared_error(sales_scaled, y_fit)
 print("")
 print("MAPE: {:.8f}, rmse: {:.8f}".format(MAPE, msqrt))
-
+date_steps = 21
 # Plot some results 
 plt.figure(figsize=(18, 10))
-plt.plot(df["date"][::7],sales_scaled[::7], label = 'true sales')
-hpd_low, hpd_high = hpdi(linreg_sample["mean"].reshape(5600,1461), prob=0.99, axis=0) # Change 5600 if you change sample drawn from mcmc
-plt.plot(df["date"][::7], y_fit[::7], label = 'predictions')
-plt.fill_between(df["date"][::7],hpd_low[::7], hpd_high[::7], alpha=0.3)
+plt.plot(df["date"][::date_steps],sales_scaled[::date_steps], label = 'true sales')
+hpd_low, hpd_high = hpdi(linreg_sample["mean"].reshape(5600,1461), prob=0.80, axis=0) # Change 5600 if you change sample drawn from mcmc
+plt.plot(df["date"][::date_steps], y_fit[::date_steps], label = 'predictions')
+plt.fill_between(df["date"][::date_steps],hpd_low[::date_steps], hpd_high[::date_steps], alpha=0.3)
 plt.legend(loc='upper left')
 plt.ylabel('sales')
 plt.xlabel('date')
+plt.xticks(rotation=45, ha="right")
 plt.title("Forecasting Sales from marketing efforts (90% HPDI)")
 plt.savefig('results/linalgo_prediction.png')
 
@@ -140,6 +141,7 @@ saturations = linreg_sample["media_saturated"]
 # ax.set(title = 'prior vs posterior distributions')
 # plt.savefig("results/prior_vs_posterior_of_media_coef.png")
 # 
+#Render confounder model
 spends=jnp.ones(14)
 jackpot_category=jnp.arange(1,15)
 sales=jnp.ones(14)
@@ -148,8 +150,9 @@ index=jnp.arange(1,15)
 
 numpyro.render_model(bayes_confounder_model,model_args=(jackpot_category,spends,sales,dayofyear,index), filename='models/confounding.png', render_params=True)
 
-algo = NUTS(bayes_confounder_model, step_size=0.002, target_accept_prob=0.9, adapt_step_size=True)
-mcmc = MCMC(algo, num_samples=700, num_warmup=300,num_chains=8,chain_method='parallel')
+#Run mcmc for confounder using the same data as before
+confounder_algo = NUTS(bayes_confounder_model, step_size=0.002, target_accept_prob=0.9, adapt_step_size=True)
+mcmc = MCMC(confounder_algo, num_samples=700, num_warmup=300,num_chains=8,chain_method='parallel')
 
 mcmc.run(rng_key, **model_args)
 linreg_sample=mcmc.get_samples()

gendata.py
@@ -31,7 +31,11 @@ n = len(df)
 
 # Simulate media cost feature using uniform distribution
 media_cost = np.random.uniform(0,1, size=n)
-df["media_cost"] = 100*np.where(media_cost >0.3, media_cost, media_cost/4)
+# Generate a jackpot sizes for the model and jackpot effects regarding those.
+jackpotsizes = jackpotgenerator(df["date"].tolist())
+df["jackpot_size"] = jackpotsizes
+#incorporate jackpot size in media costs
+df["media_cost"] = 10*np.where(media_cost >0.4, media_cost, media_cost/4)*jackpotsizes*0.4
 # visualize the media cost using only sundays (does not matter cause the data is uniform)
 df_subset = df[df["day_of_week"] == 7]
 
@@ -44,12 +48,12 @@ plt.savefig('datagen_images/media_cost.png')
 #initialize the adstock effects you want
 alpha = 0.5
 # give the length of adstock effect in days. In veikkaus, this is quite small, since there is all the time new stuff coming
-l = 14
+l = 8
 geometric_adstock_transformer = GeometricAdstockTransformer(alpha=alpha, l=l)
 
 # add transformed variables to data
 
-df["media_cost_adstock"] = geometric_adstock_transformer.transform(X=df["media_cost"])
+df["media_cost_adstock"] = geometric_adstock_transformer.transform(X=df["media_cost"].values)
 
 # Plot and save the transformed data
 
@@ -72,13 +76,13 @@ plt.savefig('datagen_images/media_cost_adstock_vs_without.png')
 
 # logistic saturation transformation for the media costs
 
-mus = [1e-2,2e-2,3e-2,4e-2]
+mus = [1e-2,1.5e-2,2e-2,2.5e-2,3e-2]
 fig, ax = plt.subplots(figsize=(7, 6))
 
 for mu in mus:
     logistic_saturation_transformer = LogisticSaturationTransformer(mu=mu)
 
-    df[f"media_cost_saturation_mu{mu:.2f}"] = logistic_saturation_transformer.fit_transform(df["media_cost_adstock"])
+    df[f"media_cost_saturation_mu{mu:.2f}"] = logistic_saturation_transformer.fit_transform(df["media_cost_adstock"].values)
 
     df_subset = df[df["day_of_week"] == 7]
 
@@ -86,7 +90,7 @@ for mu in mus:
     sns.lineplot(
         x="media_cost_adstock",
         y=f"media_cost_saturation_mu{mu:.2f}",
-        label=f"mu = {mu}",
+        label=f"mu = {mu:.2f}",
         data=df_subset,
         ax=ax
     )
@@ -97,7 +101,7 @@ ax.set(title=f"Saturation curve with logistic regression");
 plt.savefig('datagen_images/media_cost_saturation_curve_logistic_transform.png')
 
 # Test beta-hill transformation
-K = 100
+K = 30
 S = 6
 B = 1
 bh_transformer = BetaHillTransformation(K=K, S=S, beta=B)
@@ -140,7 +144,7 @@ plt.savefig('datagen_images/media_cost_all_transformations.png')
 
 # Apply dim return curve effect
 
-df["beta"] =  (np.arange(start=0.0, stop=1.0, step=1/n) + 1) ** (-1.8)
+df["beta"] =  (np.arange(start=0.0, stop=1.0, step=1/n) + 1) ** (-1.2)
 df_subset = df[df["day_of_week"] == 7]
 
 fig, ax = plt.subplots()
@@ -202,9 +206,9 @@ plt.savefig(f'datagen_images/media_cost_effect_ratio.png')
 # Simulate trend and seasonality
 df["trend"] = (np.linspace(start=0.0, stop=50, num=n) + 10)**(1/3) - 1 
 
-df["cs"] = - np.sin(1 * 2 * np.pi * df["dayofyear"] / 365.5) 
-df["cc"] = np.cos(2 * 2 * np.pi * df["dayofyear"] / 365.5)
-df["seasonality"] = 0.2 * (df["cs"] + df["cc"])
+df["cs"] = - np.sin(2 * 2 * np.pi * df["dayofyear"] / 365.5) 
+df["cc"] = np.cos(1 * 2 * np.pi * df["dayofyear"] / 365.5)
+df["seasonality"] = 0.3 * (df["cs"] + df["cc"])
 df_subset = df[df["day_of_week"] == 7]
 
 fig, ax = plt.subplots()
@@ -217,36 +221,34 @@ plt.savefig(f'datagen_images/trend_seasonality.png')
 #Create intercept variable and noise for the data
 np.random.seed(seed)
 
-# Generate a jackpot sizes for the model and jackpot effects regarding those.
-jackpotsizes = jackpotgenerator(df["date"].tolist())
-df["jackpot_size"] = jackpotsizes
+
 
 
 # Generate a jackpot size effect for the media effect(Confounder)
 # media_effect_with_jackpot = jackpot_size * media_effect/3, therefore the media will be worse when jackpot size is small
-df.eval(expr="media_effect_with_jp = media_effect * jackpot_size*0.2", inplace=True)
 
-#Plot scatterplot correlation between jackpot size and media_effect_with_jp
+
+#Plot scatterplot correlation between jackpot size and media cost
 fig, ax = plt.subplots()
-sns.scatterplot(data=df, x="jackpot_size", y="media_effect_with_jp", ax=ax, hue='jackpot_size')
+sns.scatterplot(data=df, x="jackpot_size", y="media_cost", ax=ax, hue='jackpot_size')
 ax.set(title = 'Correlation between media effect and jackpot size')
-plt.savefig(f'datagen_images/correlation_jackpot_size_media_effect.png')
+plt.savefig(f'datagen_images/correlation_jackpot_size_media_cost.png')
 
 df_subset = df[df["day_of_week"] == 7]
 fig, ax = plt.subplots()
 sns.lineplot(x="date", y="media_effect", color="C2", label="media effect", data=df_subset, ax=ax)
-sns.lineplot(x="date", y="media_effect_with_jp", color="C8", label="media effect with jackpot size", data=df_subset, ax=ax)
+sns.lineplot(x="date", y="media_cost", color="C8", label="media cost", data=df_subset, ax=ax)
 ax.legend(loc="upper left")
 ax.set(title="Media effect with jackpot size", ylabel="effect")
-plt.savefig(f'datagen_images/mediaeffect_w_jackpot.png')
+plt.savefig(f'datagen_images/mediaeffect_and_costs.png')
 
 df["intercept"] = 3.0
 df["trend_plus_intercept"] = df["trend"] + df["intercept"]
-
+amplitude = 10
 # the noise variance is increasing to make sure the resulting time series has constant variance
 sigma_epsilon  = np.linspace(start=3e-2, stop=7e-2, num=n)
 df["epsilon"] = np.random.normal(loc=0.0, scale=sigma_epsilon)
-df.eval(expr="sales = intercept + trend + seasonality + media_effect_with_jp + epsilon + jackpot_size", inplace=True)
+df.eval(expr=f"sales = {amplitude}*(intercept + trend + seasonality + media_effect + epsilon + jackpot_size)", inplace=True)
 
 df_subset = df[df["day_of_week"] == 7]
 fig, ax = plt.subplots()
@@ -265,3 +267,13 @@ df.to_csv('data/data.csv', sep=';', index=True)
 print("")
 print("DATA SAVED!")
 print("-----------")
+# Calculate ROAS from the data generated
+df.eval(expr=f"sales_without_channel = {amplitude}*(intercept + trend + seasonality + epsilon + jackpot_size)", inplace=True)
+sales_without_channel = df["sales_without_channel"]
+df.eval(expr=f"ROAS = sales - sales_without_channel ", inplace=True)
+roas_mean = df["ROAS"].mean()
+fig, ax = plt.subplots()
+df_subset = df[df["day_of_week"] == 7]
+sns.lineplot(x="date", y="ROAS", color='red', data=df_subset, ax=ax, label=f"Mean: {roas_mean:.2f}")
+ax.set(title="Estimated ROAS")
+plt.savefig("datagen_images/ROAS.png")

models/models.py
@@ -11,6 +11,7 @@ from utils import GeometricAdstockTransformer, LogisticSaturationTransformer, Be
 import numpyro
 import numpyro.distributions as dist
 from numpyro.infer import MCMC, NUTS
+from numpyro.infer.reparam import TransformReparam
 from numpyro import plate
 
 devices = config.devices
@@ -36,8 +37,6 @@ def bayes_linreg_model(jackpot_category,spends, sales, dayofyear, index):
     # !!!!! PRIOR DISTRIBUTIONS !!!!!!
     # j-pot priors
     n_jackpot_categories = len(np.unique(jackpot_category)) # infer number of categories
-    with plate("jackpot_plate", n_jackpot_categories):  # Use a plate for categories
-        c_jackpot = numpyro.sample('c_jackpot', dist.Normal(0, 10)) # Prior for each category
 
     #trend
     trend_coef = numpyro.sample("trend_coef", dist.Normal(0, 4))
@@ -57,27 +56,34 @@ def bayes_linreg_model(jackpot_category,spends, sales, dayofyear, index):
 
     #!!! COMPONENT PARAMETERS !!!
     # Trend component
-    trend = numpyro.deterministic("trend", trend_transform(trend_coef))
     seasonality_effect = numpyro.sample("seasonality_effect", dist.Uniform(0, 10))
     # Seasonality
     n_days = 365  # Maximum possible days in a year
-    seasonality = numpyro.deterministic("seasonality", fourier_modes(seasonality_effect))
+    
 
 
     geometric_adstock=GeometricAdstockTransformer(alpha=alpha, l=l_max)
     logistic_saturation=LogisticSaturationTransformer(mu=lam) 
     # Jackpot
     with plate("jackpot_plate", n_jackpot_categories):
+        c_jackpot = numpyro.sample('c_jackpot', dist.Uniform(-2,2)) 
         jackpot_effect = c_jackpot[jackpot_category]
         jackpot = numpyro.deterministic("jackpot",jnp.dot(jackpot_effect,jackpot_category))
 
-    # Media spend
     rev_sigma = numpyro.sample("sigma", dist.Uniform(0, 5))
-    media_adstock = numpyro.deterministic("media_adstock", geometric_adstock.transform(X = spends))
-    media_saturated = numpyro.deterministic("media_saturated", logistic_saturation.transform(X = media_adstock))
-    media_effect = numpyro.deterministic("media_effect",jnp.dot(media_coef, media_saturated))
-    rev_mean = numpyro.deterministic("mean", jackpot + media_effect + trend + seasonality)
-    sales = numpyro.sample('sales', dist.Normal(rev_mean, rev_sigma), obs=sales)
+    # Media spend
+    with plate("date_plate", len(index)):
+        spends = numpyro.sample("spends", dist.Normal(0,1), obs=spends)
+        #media_adstock = numpyro.deterministic("media_adstock", geometric_adstock.transform(X = spends))
+        #media_saturated = numpyro.deterministic("media_saturated", logistic_saturation.transform(X = media_adstock))
+        media_effect = numpyro.deterministic("media_effect", jnp.dot(media_coef, spends))
+
+        trend = numpyro.deterministic("trend", trend_transform(trend_coef))
+        seasonality = numpyro.deterministic("seasonality", fourier_modes(seasonality_effect))
+        rev_mean = numpyro.deterministic("mean", jackpot + media_effect + trend + seasonality)
+        sales = numpyro.sample("sales", dist.StudentT(rev_mean, rev_sigma), obs=sales)
+
+
 
 
 
@@ -111,7 +117,7 @@ def bayes_confounder_model(jackpot_category,spends, sales, dayofyear, index):
 
     
     # Media spend prior
-    media_coef = numpyro.sample('media_coef', dist.HalfNormal(2))
+
 
     #Normal priors
     alpha= numpyro.sample('alpha', dist.Gamma(2,3))
@@ -133,14 +139,17 @@ def bayes_confounder_model(jackpot_category,spends, sales, dayofyear, index):
     logistic_saturation=LogisticSaturationTransformer(mu=lam) 
     # Jackpot
     with plate("jackpot_plate", n_jackpot_categories):
+        media_coef = numpyro.sample('media_coef', dist.HalfNormal(2))
         jackpot_effect = c_jackpot[jackpot_category]
         jackpot = numpyro.deterministic("jackpot",jnp.dot(jackpot_effect,jackpot_category))
+        media_adstock = numpyro.deterministic("media_adstock", geometric_adstock.transform(X = spends)) # The spends should be wrapped with different jackpot categories to represent in a dim [spends, jackpot]
+        media_saturated = numpyro.deterministic("media_saturated", logistic_saturation.transform(X = media_adstock))
+        media_only = numpyro.deterministic("media_only",jnp.dot(media_coef, media_saturated))
+        media_effect = numpyro.deterministic("media_effect",jnp.dot(jackpot_media_effect,media_only))
+
 
     # Media spend
     rev_sigma = numpyro.sample("sigma", dist.Uniform(0, 5))
     #TODO: Check how to do media saturation etc correctly. Should these be in the jackpot plate or where?
-    media_adstock = numpyro.deterministic("media_adstock", geometric_adstock.transform(X = spends))
-    media_saturated = numpyro.deterministic("media_saturated", logistic_saturation.transform(X = media_adstock))
-    media_effect = numpyro.deterministic("media_effect",jnp.dot(jackpot_media_effect,jnp.dot(media_coef, media_saturated)))
     rev_mean = numpyro.deterministic("mean", jackpot + media_effect + trend + seasonality)
     sales = numpyro.sample('sales', dist.Normal(rev_mean, rev_sigma), obs=sales)

utils.py
@@ -65,23 +65,26 @@ class BetaHillTransformation(BaseEstimator, TransformerMixin):
 
 
 def jackpotgenerator(datelist):
-    for i in datelist:
+    for idx, i in enumerate(datelist):
         if i ==datelist[0]:
             jackpot_size = [1]
         else:
-            jackpot = jackpot_size[-1]
-            win_prob = np.random.lognormal(mean=0, sigma=1)
-            if win_prob > 2.0:
+            if idx % 7 ==0:
+                jackpot = jackpot_size[-1]
+                win_prob = np.random.lognormal(mean=0, sigma=1)
+                if win_prob > 2.3:
                     # pot is won, jackpot size goes to 1
                     jackpot=1
                     jackpot_size.append(jackpot)
-            else:
+                else:
                     #no win, increase jackpot size and go on
                     if jackpot == 15:
                         jackpot = 15
                     else:
                         jackpot +=1
                     jackpot_size.append(jackpot)
+            else:
+                jackpot_size.append(jackpot_size[-1])
     return jackpot_size
     
 